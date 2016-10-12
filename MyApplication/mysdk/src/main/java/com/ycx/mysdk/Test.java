5c34e0e2e96f4943ff2f9c3da4ef61b84d4b1103
李小明 <lixiaoming@ycx.net>
2016-10-12 15:55:48 +0800

    生成jar文件

---
+++ MyApplication/mysdk/src/main/java/com/ycx/mysdk/Test.java
@@ -0,0 +1,16 @@
+package com.ycx.mysdk;
+
+import android.util.Log;
+
+/**
+ * Created by 李小明 on 16/10/12.
+ * 邮箱:287907160@qq.com
+ */
+
+public class Test {
+
+
+    public static void showLogger(String src){
+        Log.i("lxm","showLogger = "+src);
+    }
+}
